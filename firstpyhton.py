f1e704299f0ef7c0b47b698bd07e8505aab7d3c3
IsmetUgur <109080683+IsmetUgur@users.noreply.github.com>
2023-04-17 13:40:56 +0300

    Update firstpyhton.py

--- firstpyhton.py
+++ firstpyhton.py
@@ -1,2 +1,21 @@
-#display the output
-print("My new Pyhton File!")
+import os
+
+os.chdir("c://Users//iugur//Desktop")
+cur_path=os.getcwd()
+
+getKelime=input("Kelimeyi giriniz: ")
+getKelime=getKelime.lower()
+count=0
+
+with open(r"Anayasa.txt", 'r') as fp:
+    for l_no, line in enumerate(fp):
+        # search string
+        if getKelime in line:
+            count+=1
+            print('\nstring found in a file')
+            print('Line Number:', l_no)
+            print('Line:', line)
+print(count," sonuç bulundu")
+            
+
+    
