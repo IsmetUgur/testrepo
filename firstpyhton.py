3c9c21d61585de850be33fb0d604b4d4b61ed79c
IsmetUgur <109080683+IsmetUgur@users.noreply.github.com>
2023-04-17 14:23:33 +0300

    Update firstpyhton.py

--- firstpyhton.py
+++ firstpyhton.py
@@ -7,7 +7,7 @@ getKelime=input("Kelimeyi giriniz: ")
 getKelime=getKelime.lower()
 count=0
 
-with open(r"Anayasa.txt", 'r') as fp:
+with open(r"tapuSicilTuzugu.txt", 'r') as fp:
     for l_no, line in enumerate(fp):
         # search string
         if getKelime in line:
@@ -15,7 +15,8 @@ with open(r"Anayasa.txt", 'r') as fp:
             print('\nstring found in a file')
             print('Line Number:', l_no)
             print('Line:', line)
+            x=line.find(getKelime)
+            print("İndex :", x)
 print(count," sonuç bulundu")
-            
 
     
